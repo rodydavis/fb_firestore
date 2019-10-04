6be390471c7cf6844509ba89243ddfaa41de224b
Rody Davis <rody.davis.jr@gmail.com>
2019-10-04 14:35:17 -0400

    removing ios and android

--- ios/Classes/FbFirestorePlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface FbFirestorePlugin : NSObject<FlutterPlugin>
-@end
--- ios/Classes/FbFirestorePlugin.m
+++
@@ -1,8 +0,0 @@
-#import "FbFirestorePlugin.h"
-#import <fb_firestore/fb_firestore-Swift.h>
-
-@implementation FbFirestorePlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFbFirestorePlugin registerWithRegistrar:registrar];
-}
-@end
